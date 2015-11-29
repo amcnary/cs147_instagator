4a1436209fe212a1ae90ea6c036f133c4f5f2c68
Amanda McNary <amanda.mcnary@stem.com>
2015-11-29 14:04:22 -0600

    added view controllers and in progress of adding table view controllers

---
+++ instagator-prototype/instagator-prototype/PollOptionTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PollActivityOptionTableViewCell.swift
+//  instagator-prototype
+//
+//  Created by Amanda McNary on 11/29/15.
+//  Copyright © 2015 ThePenguins. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PollOptionTableViewCell: UITableViewCell {
+    
+}
---
+++ instagator-prototype/instagator-prototype/PollVoteViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PollVoteViewController.swift
+//  instagator-prototype
+//
+//  Created by Amanda McNary on 11/29/15.
+//  Copyright © 2015 ThePenguins. All rights reserved.
+//
+
+import Foundation
